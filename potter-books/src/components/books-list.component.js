8a8b3ef6fb1073befa826ddff4117314ca65e699
julien chameroy <julienchameroy@MacBook-Pro-de-julien.local>
2020-04-29 19:25:57 +0200

    les titres des livres récupérés depuis api

--- potter-books/src/components/books-list.component.js
+++ potter-books/src/components/books-list.component.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const Book = (props) => (
+  <tr>
+    <td>{props.book.title}</td>
+  </tr>
+);
 
 export default class BooksList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { books: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://henri-potier.xebia.fr/books/")
+      .then((response) => {
+        this.setState({ books: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  bookList() {
+    return this.state.books.map((currentbook) => {
+      return <Book book={currentbook} key={currentbook._id} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the Books List component!</p>
+        <table className="table">
+          <thead className="thead-light">
+            <tr></tr>
+          </thead>
+          <tbody>{this.bookList()}</tbody>
+        </table>
       </div>
     );
   }
